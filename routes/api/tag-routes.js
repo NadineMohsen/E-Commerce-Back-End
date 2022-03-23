ad284510c7512fea1e6eefd8c0eef37cb73d36ec
Nadine <nadine.mohsen@hotmail.com>
2022-03-23 16:29:35 +0000

    find a single tag by its `id`

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -24,7 +24,40 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where:{
+      id:req.params.id
+    },
+    include:[
+      {
+        model:Product,
+        attributes:['id','product_name','price','stock','category_id']
+      }
+    ]
+  })
+  .then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json({ message: 'No tag found with this id'}); 
+      return; 
+    }
+    res.json(dbTagData)
+  })
+  .catch(err=>{
+    console.log(err);
+    res.status(500).json(err);
+  })
+});
 
+router.post('/', (req, res) => {
+  // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err=>{
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.post('/', (req, res) => {
